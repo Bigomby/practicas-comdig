0694559ebd5a6a2715a2ba375d29038c30f10298
Diego Fernández Barrera <bigomby@gmail.com>
2015-10-27 00:38:08 +0100

    Comentado y mejorado apartados 1 y 2

--- practica01/apartado01.m
+++ practica01/apartado01.m
@@ -1,7 +1,12 @@
 clear all;
 close all;
 
+% Número de muestras que va a tener la señal
 N = 10;
-An = 4 * randi([1,2], 1, N) - 6;
+
+% Señal con valores +/- 2
+An = 4 * randi([0,1], 1, N) - 2;
+
+% Mostramos la secuencia
 h = stem(An);
 set(h, 'linewidth', 2.0);
--- practica01/apartado02.m
+++ practica01/apartado02.m
@@ -2,7 +2,7 @@ clear all
 close all
 
 % Duración
-Tb = 10 ^ (-3);
+Tb = 1e-3;
 
 % Número de muestras
 L = 8;
@@ -10,22 +10,31 @@ L = 8;
 % Tiempo de muestreo
 Tm = Tb / L;
 
-% Eje de tiempos
-t = 0: Tm: Tb - Tm;
-n = 1: L;
-
 % Energía
 E = 1;
 
-% Señal discreta
-sn=ones(1, L);
-sn=sqrt(E/L).*sn;
+% Señal discreta y normalizada
+sn = ones(1, L);
+sn = sqrt(E/L) * sn;
 
-% Definición de la señal
-st = (1 / sqrt(Tm)).*sn;
+% Señal continua
+t = 0 : Tm : Tb - Tm;
+st = (1/sqrt(Tm)) * sn;
+
+% Añadimos un punto para t=0 y t=Tb en el que la señal valga cero
+t = [t(1)-eps t]; st = [0 st];
+t = [t t(end)+eps]; st = [st 0];
 
 % Plot digital
-subplot(2,1,1);stem(n,sn); grid; ylabel('S(n) (Discreto)'); xlabel('t (n)');
+subplot(2,1,1); h = stem(sn); grid; xlabel('Secuencia');
+set(h, 'linewidth', 2.0);
+axis([0 L+1]);
 
 % Plot analógica
-subplot(2,1,2); plot(t, st); grid; ylabel('S(t) (Continuo)'); xlabel('t (s)');
+subplot(2,1,2); s = plot(t, st); grid; xlabel('Señal');
+set(s, 'linewidth', 2.0);
+axis([-t(end)*0.1 t(end)*1.1]);
+
+% Comprobamos que ambas señales tienen la misma potencia
+Pn = sn*sn'
+Pt = st(2)^2 * Tb
