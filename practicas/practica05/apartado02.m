98db06fb3ae6c478e62c027343c090e161e6cfb5
Diego Fernández Barrera <bigomby@gmail.com>
2017-12-18 13:45:38 +0100

    Actualizar práctica 5

--- practicas/practica05/apartado02.m
+++ practicas/practica05/apartado02.m
@@ -9,24 +9,25 @@ L = 64;
 M = 4;
 K = log2(M);
 
-% Secuencia a transmitir
 Bn = [0 0 0 1 1 0 1 1];
 
-% Frecuencia de la portadora
-wc = (8 * pi) / L;
+M1 = 2;
+M2 = 2;
 
-% Pulso conformador rectangular
-gn = ones(1, L);
-
-% Pulsos bases
 n = 1:1:L;
+gn = ones(1, L);
 p1 = gn .* cos(wc * n);
 p2 = gn .* sin(wc * n);
 
-% Se transmite la señal
-[Xn, Bn, An, phi1 phi2, alfabeto] = transmisorpsk(Bn, Eb, M, p1, p2, L, pi/4);
+[Xn, BnI, BnQ, AnI, AnQ, alfabetoI, alfabetoQ, phi1, phi2] = ...
+  transmisorqam(Bn, Eb, M1, M2, p1, p2, L);
 
-Nb = length(An) * K;
+% alfabeto = [];
+% for k = 1:M1
+%   alfabeto = [alfabeto alfabetoI(k) + j * alfabetoQ];
+% end
+
+Nb = length(BnI) + length(BnQ);
 Tb = 1 / Rb;
 Ts = Tb * K;
 Tm = Ts / L;
--- practicas/practica05/apartado03.m
+++ practicas/practica05/apartado03.m
@@ -3,11 +3,11 @@ addpath('../lib/');
 clear all;
 close all;
 
-Rb = 1e3;
+Rb = 1e4;
 Eb = 4;
+wc = 8 * pi
 L = 64;
 M = 4;
-K = log2(M);
 
 % Secuencia a transmitir
 Bn = [0 0 0 1 1 0 1 1];
@@ -15,18 +15,18 @@ Bn = [0 0 0 1 1 0 1 1];
 % Frecuencia de la portadora
 wc = (8 * pi) / L;
 
-% Pulso conformador rectangular
-gn = ones(1, L);
+% Pulso conformador senoidal
+n = 1:1:L;
+gn = sin((2 * pi)/(2 * L) * n);
 
 % Pulsos bases
-n = 1:1:L;
 p1 = gn .* cos(wc * n);
 p2 = gn .* sin(wc * n);
 
 % Se transmite la señal
 [Xn, Bn, An, phi1 phi2, alfabeto] = transmisorpsk(Bn, Eb, M, p1, p2, L, pi/4);
 
-Nb = length(An) * K;
+Nb = length(An) * K
 Tb = 1 / Rb;
 Ts = Tb * K;
 Tm = Ts / L;
